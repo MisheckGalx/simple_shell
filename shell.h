e6a84d88d48fd7e37ccfee6e345f00fe70671d33
WayneRu95 <waynerumbidzai@gmail.com>
2023-11-14 10:23:27 -0800

    Shell.h'

---
+++ shell.h
@@ -0,0 +1,35 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+extern char **environ;
+
+#include <stdio.h>
+#include <stlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/****FUNCTIONS******/
+
+void ky_pr(void);
+char **ky_tokenize(char *line);
+char *ky_find_command(char **argv);
+void ky_execute(char **argv);
+void ky_ex(char *line);
+int main(void);
+int ky_exit(char **argv);
+int ky_env(cahr **argv);
+
+/****CUSTOM STRINGS*****/
+
+char *ky_strcpy(char *dest, const char *src);
+char *ky_strcat(char *dest, const char *src);
+int ky_strcmp(const char *s1, const char *s2);
+size_t ky_strcspn(const char *str1, const char *str2);
+ssize_t ky_getline(char **lineptr, size_t *n, FILE *stream);
+size_t ky_strlen(const char *s);
+char *ky_strtok(char *str, const char *delim);
+char *ky_strdup(const char *s);
+
+#endif /* SHELL_H */
